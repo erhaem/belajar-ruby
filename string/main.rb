d699e3e007c3125f1f3512f64d25147a3a90a1fa
erhaem <rifqihaidev@gmail.com>
2025-12-16 16:00:52 +0700

    feat: add string and variable

---
+++ string/main.rb
@@ -0,0 +1,18 @@
+name = "erhaem"
+age = 1000
+field = "Accounting"
+
+title_of_message = "a letter\n\n".capitalize
+
+# multiline (heredoc?) and string interpolation and length and replace (sub)
+message = <<~TEXT
+  Hello this is #{name} (#{name.length} chars)
+  #{age} years old.
+  I'm currently working in #{field.sub("Accounting", "IT")} field.
+TEXT
+
+# string escape, interpolation, upcase, repeat 3 times
+end_of_message = "\nLet's having #{"fun".upcase} yeah \\m/" * 3
+
+# string concatination
+puts title_of_message + message + end_of_message
---
+++ variable/main.rb
@@ -0,0 +1,11 @@
+# variables are dynamic, no type, let,var,const,$ keywords
+name = "erhaem" # string
+age = 25 # int
+active = true # boolean
+price = 10.5 # float
+
+puts name # puts has '\n' [newline] in the end
+print age # print has no '\n'
+
+puts active
+puts price
